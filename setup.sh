42e84b700456f0e58a4c00292fe4cb5aff82f62c
arpan <arpan.rec@outlook.com>
2021-12-17 11:35:11 +0530

    yay

--- setup.sh
+++ setup.sh
@@ -164,7 +164,7 @@ fi
 
 PKGS_AUR=('ttf-menlo-powerline-git' 'kde-thumbnailer-apk' 'resvg' 'sweet-gtk-theme-mars' 'kvantum-theme-sweet-mars' 'kvantum-theme-sweet-git' 'sweet-cursor-theme-git' 'sweet-theme-git' 'sweet-folders-icons-git' 'sweet-kde-git' 'sweet-kde-theme-mars-git' 'candy-icons-git' 'layan-kde-git' 'layan-gtk-theme-git' 'layan-cursor-theme-git' 'kvantum-theme-layan-git' 'tela-icon-theme' 'nordic-darker-standard-buttons-theme' 'nordic-darker-theme' 'kvantum-theme-nordic-git' 'sddm-nordic-theme-git' 'nordic-kde-git' 'nordic-theme-git' 'nordic-theme' 'ttf-meslo' 'google-chrome' 'brave-bin' 'timeshift' 'visual-studio-code-bin' )
 
-sudo -H -u nebula_build_user bash -c 'yay -S --noconfirm --needed "${PKGS_AUR[@]}"'
+sudo -H -u nebula_build_user bash -c "yay -S --noconfirm --needed ${PKGS_AUR[@]}"
 
 echo "--------------------------------------"
 echo "       Create User and Groups         "
