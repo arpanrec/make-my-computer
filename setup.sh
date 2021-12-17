fff61accc0e3e1c875ff29bcc0530506357416ad
arpan <arpan.rec@outlook.com>
2021-12-17 11:30:08 +0530

    command -v yay

--- setup.sh
+++ setup.sh
@@ -156,43 +156,15 @@ echo "-------------------------------------------------------"
 if ! command -v yay &> /dev/null
 then
 # Yay User
-id -u nebula_build_user &>/dev/null || useradd -s /bin/bash -m -d /home/nebula_build_user nebula_build_user
-echo "nebula_build_user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/10-nebula_build_user
-BASEDIR=$(dirname "$0")
-sudo -H -u nebula_build_user bash -c "$BASEDIR/install_yay.sh"
+    id -u nebula_build_user &>/dev/null || useradd -s /bin/bash -m -d /home/nebula_build_user nebula_build_user
+    echo "nebula_build_user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/10-nebula_build_user
+    BASEDIR=$(dirname "$0")
+    sudo -H -u nebula_build_user bash -c "$BASEDIR/install_yay.sh"
 fi
 
-PKGS_AUR=('ttf-menlo-powerline-git' 'kde-thumbnailer-apk' 'resvg'
-'sweet-gtk-theme-mars'
-'kvantum-theme-sweet-mars'
-'kvantum-theme-sweet-git'
-'sweet-cursor-theme-git'
-'sweet-theme-git'
-'sweet-folders-icons-git'
-'sweet-kde-git'
-'sweet-kde-theme-mars-git'
-'candy-icons-git'
-'layan-kde-git'
-'layan-gtk-theme-git'
-'layan-cursor-theme-git'
-'kvantum-theme-layan-git'
-'tela-icon-theme'
-'nordic-darker-standard-buttons-theme'
-'nordic-darker-theme'
-'kvantum-theme-nordic-git'
-'sddm-nordic-theme-git'
-'nordic-kde-git'
-'nordic-theme-git'
-'nordic-theme'
-'ttf-meslo'
-'google-chrome'
-'brave-bin'
-'timeshift'
-'visual-studio-code-bin'
-)
+PKGS_AUR=('ttf-menlo-powerline-git' 'kde-thumbnailer-apk' 'resvg' 'sweet-gtk-theme-mars' 'kvantum-theme-sweet-mars' 'kvantum-theme-sweet-git' 'sweet-cursor-theme-git' 'sweet-theme-git' 'sweet-folders-icons-git' 'sweet-kde-git' 'sweet-kde-theme-mars-git' 'candy-icons-git' 'layan-kde-git' 'layan-gtk-theme-git' 'layan-cursor-theme-git' 'kvantum-theme-layan-git' 'tela-icon-theme' 'nordic-darker-standard-buttons-theme' 'nordic-darker-theme' 'kvantum-theme-nordic-git' 'sddm-nordic-theme-git' 'nordic-kde-git' 'nordic-theme-git' 'nordic-theme' 'ttf-meslo' 'google-chrome' 'brave-bin' 'timeshift' 'visual-studio-code-bin' )
 
 sudo -H -u nebula_build_user bash -c 'yay -S --noconfirm --needed "${PKGS_AUR[@]}"'
-
 
 echo "--------------------------------------"
 echo "       Create User and Groups         "
