da4c9f169de47bf84dd42d2b6fd0885b7de1bd5d
arpan <arpan.rec@outlook.com>
2022-01-17 21:53:15 +0530

    Update common.sh

--- .dotfiles/common.sh
+++ .dotfiles/common.sh
@@ -10,11 +10,11 @@ if [ command -v javac &>/dev/null ]; then
 	javacexecpath=$(readlink -f "$(which javac)")
 	export JAVA_HOME=${javacexecpath::-10}
 elif [ command -v java &>/dev/null ]; then
-	echo "Java compiler not installed which is not recommended, Using java instead"
+	print "Java compiler not installed which is not recommended, Using java instead"
 	javaexecpath=$(readlink -f "$(which java)")
 	export JAVA_HOME=${javaexecpath::-9}
 else
-	echo "Java not installed, please install java"
+	print "Java not installed, please install java"
 fi
 
 if [ -f "$HOME/.local/share/maven/bin/mvn" ]; then
@@ -26,7 +26,7 @@ if command -v mvn &>/dev/null; then
 	export M2_HOME=${mvnexecpath::-8}
 	export MAVEN_HOME=${M2_HOME}
 else
-	echo "maven not installed, please install maven"
+	print "maven not installed, please install maven"
 fi
 
 export PATH=$HOME/.local/bin:$PATH
@@ -40,8 +40,8 @@ fi
 alias ls="ls --color=auto"
 alias ll="ls --color=auto -alrh"
 alias df="sudo df -h"
-#alias ssh="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+alias ssh="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 alias sftp="sftp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 alias codesudo="sudo code --user-data-dir=/tmp/vscode-root-user-data-dir --no-sandbox"
 alias config='git --git-dir="$HOME/.dotfiles/bare" --work-tree=$HOME'
-print imported common
+print "Common files executed"
