a56549b0fdc4332fca08bf8e895ea6436122438c
arpan <arpan.rec@outlook.com>
2022-01-26 00:44:17 +0530

    Update bash-zsh.sh

--- bash-zsh.sh
+++ bash-zsh.sh
@@ -375,12 +375,12 @@ cat <<EOT > "$PATH_TO_LOCAL_PREFX/bin/code"
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
 # test that VSCode wasn't installed inside WSL
-if grep -qi Microsoft /proc/version && [ -z "$DONT_PROMPT_WSL_INSTALL" ]; then
+if grep -qi Microsoft /proc/version && [ -z "\$DONT_PROMPT_WSL_INSTALL" ]; then
 	echo "To use Visual Studio Code with the Windows Subsystem for Linux, please install Visual Studio Code in Windows and uninstall the Linux version in WSL. You can then use the \`code\` command in a WSL terminal just as you would in a normal command prompt." 1>&2
 	printf "Do you want to continue anyway? [y/N] " 1>&2
 	read -r YN
-	YN=$(printf '%s' "$YN" | tr '[:upper:]' '[:lower:]')
-	case "$YN" in
+	YN=\$(printf '%s' "\$YN" | tr '[:upper:]' '[:lower:]')
+	case "\$YN" in
 		y | yes )
 		;;
 		* )
@@ -391,38 +391,38 @@ if grep -qi Microsoft /proc/version && [ -z "$DONT_PROMPT_WSL_INSTALL" ]; then
 fi
 
 # If root, ensure that --user-data-dir or --file-write is specified
-if [ "$(id -u)" = "0" ]; then
-	for i in "$@"
+if [ "\$(id -u)" = "0" ]; then
+	for i in "\$@"
 	do
-		case "$i" in
+		case "\$i" in
 			--user-data-dir | --user-data-dir=* | --file-write )
 				CAN_LAUNCH_AS_ROOT=1
 			;;
 		esac
 	done
-	if [ -z $CAN_LAUNCH_AS_ROOT ]; then
+	if [ -z \$CAN_LAUNCH_AS_ROOT ]; then
 		echo "You are trying to start Visual Studio Code as a super user which isn't recommended. If this was intended, please specify an alternate user data directory using the \`--user-data-dir\` argument." 1>&2
 		exit 1
 	fi
 fi
 
-if [ ! -L "$0" ]; then
+if [ ! -L "\$0" ]; then
 	# if path is not a symlink, find relatively
-	VSCODE_PATH="$(dirname "$0")/.."
+	VSCODE_PATH="\$(dirname "\$0")/.."
 else
 	if command -v readlink >/dev/null; then
 		# if readlink exists, follow the symlink and find relatively
-		VSCODE_PATH="$(dirname "$(readlink -f "$0")")/.."
+		VSCODE_PATH="\$(dirname "\$(readlink -f "\$0")")/.."
 	else
 		# else use the standard install location
-		VSCODE_PATH="$PATH_TO_LOCAL_PREFX/share/vscode/code"
+		VSCODE_PATH="\$PATH_TO_LOCAL_PREFX/share/vscode/code"
 	fi
 fi
 
-ELECTRON="$VSCODE_PATH/code"
-CLI="$VSCODE_PATH/resources/app/out/cli.js"
-ELECTRON_RUN_AS_NODE=1 "$ELECTRON" "$CLI" "$@"
-exit $?
+ELECTRON="\$VSCODE_PATH/code"
+CLI="\$VSCODE_PATH/resources/app/out/cli.js"
+ELECTRON_RUN_AS_NODE=1 "\$ELECTRON" "\$CLI" "\$@"
+exit \$?
 EOT
 
 chmod +x "$PATH_TO_LOCAL_PREFX/bin/code"
