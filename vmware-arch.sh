a8a57518ead860451981ae80b3e2da338b9906d6
arpan <arpan.rec@outlook.com>
2022-01-28 01:15:46 +0530

    mkubecek vmware-host-modules

--- vmware-arch.sh
+++ vmware-arch.sh
@@ -1,15 +1,43 @@
 #!/usr/bin/env bash
-pacman -S --needed --noconfirm fuse2 gtkmm linux-headers ncurses libcanberra pcsclite
-mkdir /etc/init.d/ /tmp/nebula -p
-if [ ! -f "/tmp/nebula/vmware.bundle" ]; then
-  wget --no-clobber --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" --no-check-certificate -O /tmp/nebula/vmware.bundle https://www.vmware.com/go/getWorkstation-linux
-  # rm -rf /tmp/nebula/vmware.bundle
-fi
-chmod +x /tmp/nebula/vmware.bundle
-/bin/sh /tmp/nebula/vmware.bundle
+set -e
 
-modprobe -a vmw_vmci vmmon
+pacman -S --needed --noconfirm dkms fuse2 gtkmm3 gtkmm libaio linux-headers ncurses libcanberra pcsclite hicolor-icon-theme gtk3 gcr
+
+tmp_vmware_dir=/tmp/vmware_install_make_my_computer
+
+mkdir /etc/init.d/ $tmp_vmware_dir -p
+if [ ! -f "$tmp_vmware_dir/vmware.bundle" ]; then
+  wget --no-clobber --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" --no-check-certificate -O $tmp_vmware_dir/vmware.bundle https://www.vmware.com/go/getWorkstation-linux
+  # rm -rf $tmp_vmware_dir/vmware.bundle
+fi
+
+chmod +x $tmp_vmware_dir/vmware.bundle
+/bin/sh $tmp_vmware_dir/vmware.bundle
+
+vmware_version_installed=$(vmware --version | awk '{ print $3 }')
+current_kernel_version=$(uname -r | awk -F . '{print $1 "." $2}')
+
+if [ ! -f "$tmp_vmware_dir/w$vmware_version_installed-k$current_kernel_version.tar.gz" ]; then
+    wget "https://github.com/mkubecek/vmware-host-modules/archive/refs/tags/w$vmware_version_installed-k$current_kernel_version.tar.gz" \
+                -O "$tmp_vmware_dir/w$vmware_version_installed-k$current_kernel_version.tar.gz"
+fi
+
+mkdir -p "$tmp_vmware_dir/vmware-host-modules-w$vmware_version_installed-k$current_kernel_version"
+
+if [ ! "$(ls -A $tmp_vmware_dir/vmware-host-modules-w$vmware_version_installed-k$current_kernel_version)" ]; then
+  tar -zxvf "$tmp_vmware_dir/w$vmware_version_installed-k$current_kernel_version.tar.gz" -C "$tmp_vmware_dir/"
+fi
+
+cd "$tmp_vmware_dir/vmware-host-modules-w$vmware_version_installed-k$current_kernel_version"
+
+tar -cf vmmon.tar vmmon-only
+tar -cf vmnet.tar vmnet-only
+
+cp -v vmmon.tar vmnet.tar /usr/lib/vmware/modules/source/
+
 vmware-modconfig --console --install-all
+modprobe -a vmw_vmci vmmon
+
 cat <<EOT > "/etc/systemd/system/vmware.service"
 [Unit]
 Description=VMware daemon
@@ -75,4 +103,5 @@ WantedBy=multi-user.target
 EOT
 
 systemctl daemon-reload
-modprobe -a vmw_vmci vmmon
+
+echo "VMWare workstation install complete"
