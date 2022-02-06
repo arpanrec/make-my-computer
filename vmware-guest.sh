bdb3277797f0458896a5c2b6aa682b81205a4d73
arpan <me@arpanrec.com>
2022-02-07 02:10:20 +0530

    transmission and htop

--- vmware-guest.sh
+++ vmware-guest.sh
@@ -8,17 +8,20 @@ sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/p
 rm -f packages.microsoft.gpg
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb
 
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo add-apt-repository ppa:transmissionbt/ppa -y
 
 sudo add-apt-repository multiverse -y
 
 sudo apt update
 
-sudo apt-get install -y apt-transport-https git dkms gnupg2 curl zsh neovim
+sudo apt-get install -y apt-transport-https git dkms gnupg2 curl zsh neovim terminator \
+    transmission-gtk htop
 
 sudo apt-get install -y linux-firmware linux-headers-$(uname -r) dkms \
 network-manager net-tools build-essential openssh-server \
 dkms dhcpcd5
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # Install VSCode
 sudo apt-get install code -y
@@ -42,3 +45,6 @@ sudo systemctl enable NetworkManager
 sudo systemctl enable dhcpcd
 sudo systemctl enable ssh
 
+rm -rf google-chrome-stable_current_amd64.deb
+
+echo "END Of Script"
