e85e294be1a3b7b9b7cda43f6cd1edefef489da5
arpan <me@arpanrec.com>
2022-02-21 22:08:48 +0530

    removed extras

--- arch/kde-extras.sh
+++ arch/kde-extras.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-pack_extars+=('python-packaging' 'fuse' 'fuse2' 'fuse3')
 pack_extars+=('xclip' 'xsel' 'wl-clipboard')
-pack_extars+=('lm_sensors')
-pack_extars+=('gnu-free-fonts' 'hunspell' 'hunspell-en_us' 'hunspell-en_gb' 'sonnet') # For some spelling check
-pack_extars+=('cryfs' 'encfs' 'gocryptfs')                                            # For kde vault
-pack_extars+=('taglib')                                                               #  Audio meta data editor
-pack_extars+=('ghostscript' 'libxml2' 'jasper' 'texlive-core' 'libwmf' 'scour' 'pstoedit' 'fig2dev')
+pack_extars+=('hunspell-en_us' 'hunspell-en_gb') # For some spelling check
+pack_extars+=('cryfs' 'encfs' 'gocryptfs')       # For kde vault
+pack_extars+=('texlive-core' 'libwmf' 'scour' 'pstoedit' 'fig2dev')
 pack_extars+=('gtk-engine-murrine' 'gtk-engines')
 pack_extars+=('libotr')
 pack_extars+=(
-    'gsfonts' 'apparmor' 'xsane' 'imagescan' 'sane' 'ghostscript'
-    'dbus-python' 'python-pyqt5' 'python-gobject' 'python-pysmbc'
+    'gsfonts' 'apparmor' 'xsane' 'imagescan'
+    'python-pysmbc'
 )
 pack_extars+=('htop' 'mlocate' 'inetutils' 'net-tools')
 pack_extars+=('discord' 'gimp' 'webkit2gtk' 'gnuplot' 'sysstat')
 
-pacman -S --needed "${pack_extars[@]}"
+pacman -S --needed --noconfirm "${pack_extars[@]}"
