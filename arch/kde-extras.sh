135194943e3fc803fb25db26d011af5f2da5a64f
arpan <me@arpanrec.com>
2022-02-22 01:17:26 +0530

    appmenu-gtk-module

--- arch/kde-extras.sh
+++ arch/kde-extras.sh
@@ -5,7 +5,7 @@ pack_extars+=('xclip' 'xsel' 'wl-clipboard')
 pack_extars+=('hunspell-en_us' 'hunspell-en_gb') # For some spelling check
 pack_extars+=('cryfs' 'encfs' 'gocryptfs')       # For kde vault
 pack_extars+=('texlive-core' 'libwmf' 'scour' 'pstoedit' 'fig2dev')
-pack_extars+=('gtk-engine-murrine' 'gtk-engines')
+pack_extars+=('gtk-engine-murrine' 'gtk-engines' 'appmenu-gtk-module')
 pack_extars+=('libotr')
 pack_extars+=(
     'gsfonts' 'apparmor' 'xsane' 'imagescan'
