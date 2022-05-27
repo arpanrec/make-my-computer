b5583b41740192d0e1511e3c9429f21dd03ecf1a
arpan m <me@arpanrec.com>
2022-05-27 14:49:22 +0530

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -11,7 +11,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -
 sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 
 sudo echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
+sudo apt install software-properties-common apt-transport-https wget ca-certificates gnupg2 -y
 sudo add-apt-repository multiverse -y
 
 sudo apt update
