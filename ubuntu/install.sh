4170168e667caa355ca7b9486555c3cd6d96747d
arpan <me@arpanrec.com>
2022-05-27 16:54:38 +0530

    google-chrome is installed

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -6,7 +6,6 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >pac
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb
 
 sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 
@@ -23,7 +22,13 @@ sudo apt-get install -y linux-firmware linux-headers-"$(uname -r)" dkms \
     network-manager net-tools build-essential openssh-server \
     dkms dhcpcd5
 
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+if hash google-chrome-stable &>/dev/null; then
+	echo "google-chrome is installed!"
+else
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    rm -rf google-chrome-stable_current_amd64.deb
+fi
 
 # Install
 # sudo apt-get install code -y # Install from userapps
@@ -53,7 +58,5 @@ sudo apt install -y fuse
 sudo systemctl enable NetworkManager
 sudo systemctl enable dhcpcd
 sudo systemctl enable ssh
-
-rm -rf google-chrome-stable_current_amd64.deb
 
 echo "END Of Script"
