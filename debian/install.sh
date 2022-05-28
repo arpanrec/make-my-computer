983266553c4546837ea4ad7a6cb5dc3ab3d5dd63
arpan <me@arpanrec.com>
2022-05-29 00:05:03 +0530

    sublime-text

--- debian/install.sh
+++ debian/install.sh
@@ -14,12 +14,15 @@ deb http://deb.debian.org/debian bullseye-updates main contrib non-free
 deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
 EOT
 
+sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+sudo echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
 apt update
 
 apt-get install -y firmware-linux-free firmware-linux-nonfree linux-headers-"$(uname -r)" dkms network-manager dhcpcd5 \
     network-manager net-tools build-essential openssh-server
 
-sudo apt-get install fonts-liberation software-properties-common apt-transport-https wget ca-certificates gnupg2 -y
+sudo apt-get install fonts-liberation software-properties-common apt-transport-https wget ca-certificates gnupg2 vim sublime-text -y
 
 systemctl set-default graphical.target
 
