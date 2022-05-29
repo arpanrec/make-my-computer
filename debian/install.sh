32cef892ba4ed68da1081199977f3eb63d0982fa
arpan <me@arpanrec.com>
2022-05-29 13:08:22 +0530

    remove sudo

--- debian/install.sh
+++ debian/install.sh
@@ -14,32 +14,32 @@ deb http://deb.debian.org/debian bullseye-updates main contrib non-free
 deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
 EOT
 
-sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
-sudo echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 
 apt update
 
 apt-get install -y firmware-linux-free firmware-linux-nonfree linux-headers-"$(uname -r)" dkms network-manager dhcpcd5 \
     network-manager net-tools build-essential openssh-server
 
-sudo apt-get install fonts-liberation software-properties-common apt-transport-https wget ca-certificates gnupg2 vim sublime-text -y
+apt-get install fonts-liberation software-properties-common apt-transport-https wget ca-certificates gnupg2 vim sublime-text -y
 
 systemctl set-default graphical.target
 
 apt-get install -y task-gnome-desktop gnome
 
-sudo apt-get install -y git gnupg2 curl zsh terminator htop
+apt-get install -y git gnupg2 curl zsh terminator htop
 
 if hash google-chrome-stable &>/dev/null; then
     echo "google-chrome is installed!"
 else
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb
-    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    dpkg -i google-chrome-stable_current_amd64.deb
     rm -rf google-chrome-stable_current_amd64.deb
 fi
 
-sudo apt-get install -y python3-pip
-sudo apt-get install -y ffmpegthumbnailer ffmpeg vlc eog heif-gdk-pixbuf heif-thumbnailer
+apt-get install -y python3-pip
+apt-get install -y ffmpegthumbnailer ffmpeg vlc eog heif-gdk-pixbuf heif-thumbnailer
 
 __optional_packages=('gnome-tweak-tool' 'gnome-tweaks' 'gnome-shell-extension-manager')
 
@@ -48,14 +48,14 @@ for i in "${__optional_packages[@]}"; do
     __apt_search=$(apt-cache search --names-only "$i")
     if [[ -n "$__apt_search" ]]; then
         echo "Installing $i"
-        sudo apt-get install -y "$i"
+        apt-get install -y "$i"
     else
         echo "No install candidate for $i"
     fi
 done
 
 
-sudo apt install -y gnome-shell-extensions gnome-shell-extension-prefs
+apt install -y gnome-shell-extensions gnome-shell-extension-prefs
 
 sed -i 's/managed=false/managed=true/' /etc/NetworkManager/NetworkManager.conf
 
