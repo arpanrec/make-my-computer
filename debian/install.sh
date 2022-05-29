bcf745d6d1086f0934b2c30226e2c683a51c17c5
arpan <me@arpanrec.com>
2022-05-29 14:15:39 +0530

    apt update

--- debian/install.sh
+++ debian/install.sh
@@ -24,6 +24,8 @@ apt-get install -y firmware-linux-free firmware-linux-nonfree linux-headers-"$(u
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 
+apt update
+
 apt-get install fonts-liberation software-properties-common apt-transport-https wget ca-certificates gnupg2 vim sublime-text -y
 
 systemctl set-default graphical.target
