ff4de0a53c855440373e0584c82127d073417ce7
arpan <me@arpanrec.com>
2022-05-29 14:14:31 +0530

    rsync

--- debian/install.sh
+++ debian/install.sh
@@ -19,7 +19,7 @@ EOT
 apt update
 
 apt-get install -y firmware-linux-free firmware-linux-nonfree linux-headers-"$(uname -r)" dkms network-manager dhcpcd5 \
-    network-manager net-tools build-essential openssh-server gnupg2 wget rync
+    network-manager net-tools build-essential openssh-server gnupg2 wget rsync
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
